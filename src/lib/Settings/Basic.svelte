21461e149dd06d79376819e84a003d587df64231
RagnarVdB <ragnar.van.den.broeck@gmail.com>
2022-04-03 16:10:28 +0200

    basic sliders

--- src/lib/Settings/Basic.svelte
+++ src/lib/Settings/Basic.svelte
@@ -1,3 +1,31 @@
+<script lang="ts">
+    import Slider from '@bulatdashiev/svelte-slider'
+    $: settings = {
+        exposure: exposure[0],
+        contrast: contrast[0]
+    }
+
+    let exposure = [1, 0]
+    let contrast = [1, 0]
+    let temperature = [1, 0]
+    let tint = [1, 0]
+</script>
+
+
 <div class="basic">
-    <h1>Basic</h1>
+    exposure: {exposure[0]}
+    <Slider bind:value={exposure}/>
+
+    contrast: {contrast[0]}
+    <Slider bind:value={contrast}/>
+
+    temperature: {temperature[0]}
+    <Slider bind:value={temperature}/>
+
+    tint: {tint[0]}
+    <Slider bind:value={tint}/>
 </div>
+
+<style>
+
+</style>
