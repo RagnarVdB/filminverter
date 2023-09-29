291ba941e36c2ffe6a0c786a3c8cd5689c7d1895
RagnarVdB <ragnar.van.den.broeck@gmail.com>
2023-09-29 23:26:55 +0200

    scroll in settings

--- src/lib/Settings/Settings.svelte
+++ src/lib/Settings/Settings.svelte
@@ -100,6 +100,8 @@
         background-color: white;
         box-shadow: 0px 0px 15px rgba(0, 0, 0, 0.25);
         border-radius: 15px;
+        overflow-y: scroll;
+        padding-bottom: 10px;
     }
 
     .menu {
